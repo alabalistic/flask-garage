24ba4e2364c778a0aaf3b5aa53f5fac0b1a6a813
jumpstartstefan <stefan.softuni@gmail.com>
2024-07-22 09:34:02 +0300

    brrranch

---
+++ migrations/versions/075031cc6d2d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 075031cc6d2d
+Revises: 99fe993daf01
+Create Date: 2024-07-22 09:15:35.346639
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '075031cc6d2d'
+down_revision = '99fe993daf01'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
